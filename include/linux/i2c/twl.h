0447f73df696cc0c3bc98684c6d4cf934a309c1e
Graeme Gregory <gg@slimlogic.co.uk>
2012-02-16 16:02:09 +0000

    twl6030_bci_battery remove use_eeprom_config
    In all sensible use cases use_eeprom_config and use_hw_charger are
    actually synonymous so simplify the code and remove the use_eeprom_config.
    
    Change-Id: Ibf473ea8106a72038f646e9163665caa91de9e06
    Signed-off-by: Graeme Gregory <gg@slimlogic.co.uk>

--- include/linux/i2c/twl.h
+++ include/linux/i2c/twl.h
@@ -674,7 +674,7 @@ struct twl4030_bci_platform_data {
 	unsigned int use_hw_charger;
 	unsigned int use_power_path;
 	unsigned long features;
-	unsigned int use_eeprom_config;
+
 	unsigned long errata;
 
 	/* LGE_SJIT 2012-01-25 [dojip.kim@lge.com]
