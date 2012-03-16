801982b7eeb23787cdf4217bcc8cfe4f3346b61a
Lajos Molnar <lajos@ti.com>
2012-03-15 21:38:57 -0500

    OMAPDSS: DSSCOMP: Added platform_info and getter
    Get platform information from dss2 driver and android_display,
    instead of it being hardcoded in dsscomp clients.
    
    Change-Id: Icc23245ec93d93dd0d9b9fcd791e546720699dcc
    Signed-off-by: Lajos Molnar <lajos@ti.com>
    Signed-off-by: Dima Svetlov <svetlov@ti.com>

--- drivers/video/omap2/dsscomp/device.c
+++ drivers/video/omap2/dsscomp/device.c
@@ -39,17 +39,22 @@
 
 #include <video/omapdss.h>
 #include <video/dsscomp.h>
+#include <plat/android-display.h>
 #include <plat/dsscomp.h>
 #include "dsscomp.h"
+#include "../dss/dss_features.h"
+#include "../dss/dss.h"
 #if defined(CONFIG_MACH_LGE_COSMO_3D_DISPLAY) //##hwcho_20120522
 #ifdef CONFIG_DSSCOMP_ADAPT
 #include "dsscomp_adapt.h"
 #endif
-#endif //##
+#endif 
 #include <linux/debugfs.h>
 
 static DECLARE_WAIT_QUEUE_HEAD(waitq);
 static DEFINE_MUTEX(wait_mtx);
+
+static struct dsscomp_platform_info platform_info;
 
 static u32 hwc_virt_to_phys(u32 arg)
 {
@@ -361,7 +366,7 @@ static long query_display(struct dsscomp_dev *cdev,
 static long check_ovl(struct dsscomp_dev *cdev,
 					struct dsscomp_check_ovl_data *chk)
 {
-	/* for now return all overlays as possible */
+	/* for now return all overlays as possstruct dsscomp_dev *cdevible */
 	return (1 << cdev->num_ovls) - 1;
 }
 
@@ -419,6 +424,32 @@ static void fill_cache(struct dsscomp_dev *cdev)
 	dev_info(DEV(cdev), "found %d displays and %d overlays, WB overlay %d\n",
 				cdev->num_displays, cdev->num_ovls,
 				cdev->wb_ovl ? 1 : 0);
+}
+
+static void fill_platform_info(struct dsscomp_dev *cdev)
+{
+	struct dsscomp_platform_info *p = &platform_info;
+
+	p->max_xdecim_1d = 16;
+	p->max_xdecim_2d = 16;
+	p->max_ydecim_1d = 16;
+	p->max_ydecim_2d = 2;
+
+	p->fclk = dss_feat_get_param_max(FEAT_PARAM_DSS_FCK);
+	/*
+	 * :TODO: for now overwrite with actual fclock as dss will not scale
+	 * fclock based on composition
+	 */
+	p->fclk = dispc_fclk_rate();
+
+	p->min_width = 2;
+	p->max_width = 2048;
+	p->max_height = 2048;
+
+	p->max_downscale = 4;
+	p->integer_scale_ratio_limit = 2048;
+
+	p->tiler1d_slot_size = tiler1d_slot_size(cdev);
 }
 
 static long comp_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
@@ -486,6 +517,13 @@ static long comp_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 	{
 		r = copy_from_user(&u.sdis, ptr, sizeof(u.sdis)) ? :
 		    setup_display(cdev, &u.sdis);
+		break;
+	}
+	case DSSCIOC_QUERY_PLATFORM:
+	{
+		/* :TODO: for now refill platform info as it is dynamic */
+		r = copy_to_user(ptr, &platform_info, sizeof(platform_info));
+		break;
 	}
 	default:
 		r = -EINVAL;
@@ -573,6 +611,7 @@ static int dsscomp_probe(struct platform_device *pdev)
        LGE_boot_compleate(1);
 
 	fill_cache(cdev);
+	fill_platform_info(cdev);
 
 	/* initialize queues */
 	dsscomp_queue_init(cdev);
@@ -582,7 +621,7 @@ static int dsscomp_probe(struct platform_device *pdev)
 #ifdef CONFIG_DSSCOMP_ADAPT
 	dsscomp_adapt_init(cdev);
 #endif
-#endif //##
+#endif
 	return 0;
 }
 
@@ -594,7 +633,7 @@ static int dsscomp_remove(struct platform_device *pdev)
 #ifdef CONFIG_DSSCOMP_ADAPT
 	dsscomp_adapt_deinit();
 #endif
-#endif //##
+#endif
 	misc_deregister(&cdev->dev);
 	debugfs_remove_recursive(cdev->dbgfs);
 	dsscomp_queue_exit();
