7a1be370d1e988cbec6ff6084134e440d587a8ec
Ruslan Bilovol <ruslan.bilovol@ti.com>
2012-06-21 19:07:43 +0300

    OMAP4: otg: phy: fix charger detection
    Charger detection feature is not correctly used
    and thus makes some regressions if OTG was used in
    the host mode. Charger detection takes 500 ms so
    if some device is attached to the host after 500 ms
    it will be suddenly resetted.
    
    This feature is enabled by default after reset/cold boot
    so always affects PHY usage, also it wasn't disabled during
    calling omap4_charger_detect. So finally this was wrongly
    interpreted as "phy requires 200ms to start" (in latest
    versions - 300ms). After fixing this we can safely remove
    uggly mdelay after powering on the PHY.
    
    Change-Id: I901d1613f1f72df408158a086ce94c554aeab186
    Signed-off-by: Ruslan Bilovol <ruslan.bilovol@ti.com>

--- arch/arm/mach-omap2/omap_phy_internal.c
+++ arch/arm/mach-omap2/omap_phy_internal.c
@@ -49,7 +49,7 @@
 #define CHARGER_TYPE_HOST		0x5
 #define CHARGER_TYPE_PC			0x6
 #define USB2PHY_CHGDETECTED		BIT(13)
-#define USB2PHY_RESTARTCHGDET BIT(15)
+#define USB2PHY_RESTARTCHGDET		BIT(15)
 #define USB2PHY_DISCHGDET		BIT(30)
 
 static struct clk *phyclk, *clk48m, *clk32k;
@@ -81,10 +81,10 @@ int omap4430_phy_init(struct device *dev)
 	/* Power down the phy */
 	__raw_writel(PHY_PD, ctrl_base + CONTROL_DEV_CONF);
 
-/* Disable charger detection by default */
+	/* Disable charger detection by default */
 	usb2phycore = omap4_ctrl_pad_readl(CONTROL_USB2PHYCORE);
 	usb2phycore |= USB2PHY_DISCHGDET;
-	omap4_ctrl_pad_writel(usb2phycore, CONTROL_USB2PHYCORE);	
+	omap4_ctrl_pad_writel(usb2phycore, CONTROL_USB2PHYCORE);
 
 	if (!dev) {
 		iounmap(ctrl_base);
@@ -212,17 +212,14 @@ int omap4_charger_detect(void)
 	u32 usb2phycore = 0;
 	u32 chargertype = 0;
 
-	/* enable charger detection and restart it */ 
-	//omap4430_phy_power(NULL, 0, 1);
-
+	/* enable charger detection and restart it */
 	usb2phycore = omap4_ctrl_pad_readl(CONTROL_USB2PHYCORE);
 	usb2phycore &= ~USB2PHY_DISCHGDET;
-
 	usb2phycore |= USB2PHY_RESTARTCHGDET;
 	omap4_ctrl_pad_writel(usb2phycore, CONTROL_USB2PHYCORE);
 	mdelay(2);
 	usb2phycore = omap4_ctrl_pad_readl(CONTROL_USB2PHYCORE);
-	usb2phycore &= ~USB2PHY_RESTARTCHGDET;	
+	usb2phycore &= ~USB2PHY_RESTARTCHGDET;
 	omap4_ctrl_pad_writel(usb2phycore, CONTROL_USB2PHYCORE);
 
 	timeout = jiffies + msecs_to_jiffies(500);
@@ -251,9 +248,9 @@ int omap4_charger_detect(void)
 		pr_info("PS/2 detected!\n");
 		break;
 	default:
-		pr_err(KERN_ERR"Unknown charger detected! %d\n", chargertype);
+		pr_err("Unknown charger detected! %d\n", chargertype);
 	}
-	//omap4430_phy_power(NULL, 0, 0);
+
 	usb2phycore = omap4_ctrl_pad_readl(CONTROL_USB2PHYCORE);
 	usb2phycore |= USB2PHY_DISCHGDET;
 	omap4_ctrl_pad_writel(usb2phycore, CONTROL_USB2PHYCORE);
@@ -305,7 +302,7 @@ int omap4430_phy_suspend(struct device *dev, int suspend)
 		/* power on the phy */
 		if (__raw_readl(ctrl_base + CONTROL_DEV_CONF) & PHY_PD) {
 			__raw_writel(~PHY_PD, ctrl_base + CONTROL_DEV_CONF);
-			//mdelay(300);
+			mdelay(300);
 		}
 
 		/* restore the context */
