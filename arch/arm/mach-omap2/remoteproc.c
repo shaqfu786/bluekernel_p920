6a2b62586d0be307e20a94ce37c60a196d81399e
shaqfu786 <shaqfu786@gmail.com>
2019-02-23 00:02:07 +0200

    Fix for Cosmo Ducati

--- arch/arm/mach-omap2/remoteproc.c
+++ arch/arm/mach-omap2/remoteproc.c
@@ -233,10 +233,10 @@ static int __init omap_rproc_init(void)
 		if (omap_total_ram_size() == SZ_512M) {
 			if (!strcmp("ipu", omap4_rproc_data[i].name))
 				omap4_rproc_data[i].firmware =
-					"ducati-m3.512MB.bin";
+					"ducati-m3.bin";
 			else if (!strcmp("dsp", omap4_rproc_data[i].name))
 				omap4_rproc_data[i].firmware =
-					"tesla-dsp.512MB.bin";
+					"tesla-dsp.bin";
 		}
 
 		oh[0] = omap_hwmod_lookup(oh_name);
